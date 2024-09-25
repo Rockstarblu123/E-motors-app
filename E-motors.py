51b12aadba4818e8ccc0910c4dfc657903830c0f
Rockastarblu123 <127698072+Rockstarblu123@users.noreply.github.com>
2024-09-25 16:23:01 -0600

    actualizacion de repositorio

--- E-motors.py
+++ E-motors.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_mysqldb import MySQL
+from flask_login import LoginManager, login_user, logout_user
 from werkzeug.security import generate_password_hash
 import datetime
-from config import config  # Importa la configuración
+from config import config# Importa la configuración
+from models.ModelUser import ModelUser
+from models.entities.User import User
 
 motors = Flask(__name__)
 motors.config.from_object(config['development'])  # Cargar la configuración
 db = MySQL(motors)
+adminSession = LoginManager(motors)
+
+@adminSession.user_loader
+def adbUser(id):
+    return ModelUser.get_by_id(db,id)
 
 @motors.route("/")
 def home():
@@ -14,7 +22,23 @@ def home():
 
 @motors.route("/signin", methods=['GET', 'POST'])
 def signin():
-    return render_template('signin.html')
+    if request.form == 'POST':
+        usuario = User(0, None, request.form['correo'], request.form['clave'], None, None)
+        usuarioAutenticado = ModelUser.signin(db, usuario)
+        if usuarioAutenticado is not None:
+            if ususarioAutenticado.clave:
+                login_user(usuarioAutenticado)
+                if usuarioAutenticado.perfil == 'A':
+                    return render_template('admin.html')
+                else:
+                    return render_template('user.html')
+            else:
+                return 'no seas ricardo pon tu contrasena bien'
+        else:
+            return  'usuario inexistente'
+    else:
+        return render_template('signin.html')
+    
 
 @motors.route('/signup', methods=['GET', 'POST'])
 def signup():
